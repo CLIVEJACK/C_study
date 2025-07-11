272a4c9da50e72e0a7cdc730459b457f17f66a04
DongKuk-Kim <yum0153@gmail.com>
2025-07-11 09:07:09 +0900

    7월10일 목요일 커밋이다 웅나

--- test.cpp/test.cpp/remp.cpp
+++ test.cpp/test.cpp/remp.cpp
@@ -1,23 +1,23 @@
-﻿#include <iostream>
-
-//int add(int a, int b)		// 다중정의보다는 템플릿을 사용해라 함수 다형성을이용해서 나타냄
-//{
-//	return a + b;
-//}
+﻿//#include <iostream>
 //
-//double add(double a, double b)
+////int add(int a, int b)		// 다중정의보다는 템플릿을 사용해라 함수 다형성을이용해서 나타냄
+////{
+////	return a + b;
+////}
+////
+////double add(double a, double b)
+////{
+////	return a + b;
+////}
+//
+//template <typename T>	// 함수 템릿-함수다중정의보다는 템플릿으로
+//T add(T a, T b)
 //{
 //	return a + b;
 //}
-
-template <typename T>	// 함수 템릿-함수다중정의보다는 템플릿으로
-T add(T a, T b)
-{
-	return a + b;
-}
-int main()
-{
-	std::cout << add(3, 4) << std::endl;
-	std::cout << add(3.3, 4.4) << std::endl;
-	return 0;
-}
+//int main()
+//{
+//	std::cout << add(3, 4) << std::endl;
+//	std::cout << add(3.3, 4.4) << std::endl;
+//	return 0;
+//}
